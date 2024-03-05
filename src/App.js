fb9331ef56fd190704070d191a73461fe51c122e
syed-jaid <syed.jaid.7748@gmail.com>
2024-03-05 22:06:43 +0600

    frist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import ThankYouPage from "./components/thankYou";
+import Home from "./components/home";
+import Checkout from "./components/checkout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home></Home>} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/thankyou" element={<ThankYouPage />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
