a0d99ee6476d18369aa94186e46c955d21ba242c
syed-jaid <syed.jaid.7748@gmail.com>
2024-03-07 02:50:57 +0600

    jahid-dev-responsive-form-font-ui-update

--- src/components/core/Video.js
+++ src/components/core/Video.js
@@ -10,6 +10,8 @@ const Video = () => {
         cloudinaryRef.current = window.cloudinary;
         cloudinaryRef.current.videoPlayer(videoRef.current, {
             cloud_name: 'dtl6yswho',
+            autoplay: true, // Enable autoplay
+            muted: true, // Mute the video to increase the likelihood that autoplay will work
         });
     }, []);
 
@@ -20,7 +22,7 @@ const Video = () => {
                 ref={videoRef}
                 data-cld-public-id='ngybfndymeh4dak9ywae'
                 controls
-            // className="responsive-video" // Use className for styling
+                muted={true} // Ensure the video element itself is muted as well
             />
         </div>
     );
